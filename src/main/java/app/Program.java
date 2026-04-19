302d1d5ad67562150ba05089709e17b4824fae2f
GermanEscobedo <Lautaro.Escobedo@alu.frt.utn.edu.ar>
2026-04-19 01:54:29 -0300

    Implementacion de Ventana Menu Principal

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -2,12 +2,15 @@ package app;
 
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
-import views.ListarVehiculosView;
+import views.VentanaPrincipalView;
 
 public class Program {
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
+        VentanaPrincipalView menu = new VentanaPrincipalView();
+        menu.setVisible(true);
         Persistencia.inicializar();
-        ListarVehiculosView view = new ListarVehiculosView();
-        view.setVisible(true);
+        
+        //aqui inicia la ventana menu principal
+        
     }
 }
